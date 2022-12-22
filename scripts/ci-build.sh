3334ca52fe20c8a07f815c24cd1eaf8be2526172
Azeem Sajid <azeem.sajid@gmail.com>
2022-12-22 22:58:20 +0500

    Update CI. Add build script.

---
+++ scripts/ci-build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z $BUILD_DIR || -z $TAR ]]; then
+  echo "[ERR] Set BUILD_DIR and TAR."
+  exit 1
+fi
+
+CXX=${CXX:-gcc}
+STRIP=${STRIP:-strip}
+
+echo "[INF] CXX:        $CXX"
+echo "[INF] STRIP:      $STRIP"
+echo "[INF] BUILD_DIR:  $BUILD_DIR"
+echo "[INF] TAR:        $TAR"
+
+echo "[INF] Building"
+rm -rf ./"$BUILD_DIR"
+cmake -S . -B "$BUILD_DIR"
+cmake --build "$BUILD_DIR"
+$STRIP ./"$BUILD_DIR"/spancopy*
+
+echo "[INF] Compressing"
+cd ./"$BUILD_DIR"
+tar -czvf "$TAR" spancopy*
+cd ..
+ls -Gghl ./"$BUILD_DIR"/spancopy*
+
+echo "--- [DONE] ---"
