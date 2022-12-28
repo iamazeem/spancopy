bf4503bd6492aecfed398b7f51c235d85a9b7a2f
Azeem Sajid <azeem.sajid@gmail.com>
2022-12-28 15:45:32 +0500

    [CI] Update tests. Add invalid threshold test.

--- tests/test.sh
+++ tests/test.sh
@@ -11,6 +11,12 @@ ACTUAL_DEST_DIR="/tmp/actual-destination"
 rm -rf $ACTUAL_DEST_DIR
 mkdir -p $ACTUAL_DEST_DIR
 
+echo "[INF] running tests..."
+
+echo "[INF] test # 1: invalid threshold (failure)"
+"$BUILD_DIR"/spancopy --threshold 10 --source $SOURCE_DIR --destination $ACTUAL_DEST_DIR || true
+
+echo "[INF] test # 2: valid threshold (success)"
 "$BUILD_DIR"/spancopy --threshold 100 --source $SOURCE_DIR --destination $ACTUAL_DEST_DIR
 
 EXPECTED_DEST_SUBDIR=$(ls $EXPECTED_DEST_DIR | tr -d '\n')
@@ -19,3 +25,5 @@ ACTUAL_DEST_SUBDIR=$(ls $ACTUAL_DEST_DIR | tr -d '\n')
 if diff -r "$EXPECTED_DEST_DIR/$EXPECTED_DEST_SUBDIR" "$ACTUAL_DEST_DIR/$ACTUAL_DEST_SUBDIR"; then
   rm -rf $ACTUAL_DEST_DIR
 fi
+
+echo "[INF] --- [DONE] ---"
