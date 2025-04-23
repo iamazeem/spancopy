e93ddc1d56f39c76827a8b260834a56be4478cd1
Azeem Sajid <azeem.sajid@gmail.com>
2025-04-23 11:47:10 +0500

    Update tests (#9)

--- tests/test.sh
+++ tests/test.sh
@@ -11,12 +11,21 @@ ACTUAL_DEST_DIR="/tmp/actual-destination"
 rm -rf $ACTUAL_DEST_DIR
 mkdir -p $ACTUAL_DEST_DIR
 
-echo "[INF] running tests..."
+echo "[INF] Running tests..."
 
-echo "[INF] test # 1: invalid threshold (failure)"
+echo "[INF] TEST: no arguments (success)"
+"$PREFIX"/spancopy >/dev/null
+
+echo "[INF] TEST: version flag (success)"
+"$PREFIX"/spancopy --version >/dev/null
+
+echo "[INF] TEST: help flag (success)"
+"$PREFIX"/spancopy --help >/dev/null
+
+echo "[INF] TEST: invalid threshold (failure)"
 "$PREFIX"/spancopy --threshold 10 --source $SOURCE_DIR --destination $ACTUAL_DEST_DIR || true
 
-echo "[INF] test # 2: valid threshold (success)"
+echo "[INF] TEST: valid threshold (success)"
 "$PREFIX"/spancopy --threshold 100 --source $SOURCE_DIR --destination $ACTUAL_DEST_DIR
 
 EXPECTED_DEST_SUBDIR=$(ls $EXPECTED_DEST_DIR | tr -d '\n')
